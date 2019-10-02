0de81ad68c3379945d11d401175b19ad97d84fe2
YuHao <sgcob187575@gmail.com>
2019-10-02 11:03:44 +0800

    Initial Commit

---
+++ E_book/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  E_book
+//
+//  Created by 陳昱豪 on 2019/10/2.
+//  Copyright © 2019 Chen_Yu_Hao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
